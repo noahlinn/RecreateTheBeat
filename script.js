ae5d9637d44f47533fed3cb8ebc3462197a9a355
noahlinn <n.linn95@gmail.com>
2021-03-19 19:03:01 -0400

    day one

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log('lets fucking gooo')
+const gridBox = document.querySelectorAll('.gridBox')
+const submitButton = document.querySelector('#checkForWin')
+
+let currentGrid = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+
+let winGridLOneROne = [
+    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+
+const initialize = event => {
+    addClickToGrid(gridBox, currentGrid)
+}
+const addClickToGrid = (box, current) =>{
+for(let i = 0; i<box.length; i++){
+    box[i].addEventListener('click',() => {
+        if (current[i] === 0){
+        addFill(box, i, current)}
+        else{
+        removeFill(box, i, current)
+            }
+        })
+    }
+}
+submitButton.addEventListener('click', () => {
+    checkForWin(currentGrid,winGridLOneROne)
+})
+
+const checkForWin = (current, win) => {
+	for (var i = 0; i < current.length; i++) {
+		if (current[i] !== win[i]) return false;
+	}
+	console.log('shit yea')
+};
+
+const addFill = (box, i, current) => {
+    box[i].classList.add('fill')
+    current[i] = 1
+}
+const removeFill = (box, i, current) => {
+    box[i].classList.remove('fill')
+    current[i] = 0
+}
+
+
+
+document.addEventListener('DOMContentLoaded', initialize);
