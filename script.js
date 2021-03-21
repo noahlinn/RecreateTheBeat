948e909ed108b5696c366e07389b14ff30721f99
noahlinn <n.linn95@gmail.com>
2021-03-21 00:29:25 -0400

    next level conditions added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const levelTitle = document.querySelector('#levelHeader')
+const levelContainer = document.querySelector('.level')
 const gridBox = document.querySelectorAll('.gridBox')
 const submitButton = document.querySelector('#checkForWin')
 const playBackButton = document.querySelector('#playBack')
@@ -7,6 +9,16 @@ const snSound = document.querySelector('.snaresound')
 const kickSound = document.querySelector('.kicksound')
 const rideSound = document.querySelector('.ridesound')
 let playCurrentGrid = 0
+let level = 1
+let isPlaying = null
+
+
+
+// class Level {
+//     constructor(winCombintation)
+// }
+
+
 
 
 let currentGrid = [
@@ -16,22 +28,28 @@ let currentGrid = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]
 
-let winGridLOneROne = [
+const winLevel1 = [
     1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
     0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
     1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]
 
-let isPlaying = null
+const winLevel2 = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0
+]
 
-// console.log(0%16)
-// console.log(1%16)
-// console.log(31%16)
+
+// let 
+
+
 
 sequence = () =>{
     let each = playCurrentGrid % 16
-    liveBox = document.querySelectorAll(`.box${each}`)
+    liveBox = document.querySelectorAll(`.box${each}`)  
 
     liveBox.forEach((note) => {
 
@@ -63,7 +81,7 @@ sequence = () =>{
     playCurrentGrid++
     // console.log(liveBox)
 }
-
+//starts the sequence function 
 rock = () => {
     let tempo = 150;
     if (isPlaying){
@@ -76,47 +94,32 @@ rock = () => {
         }, tempo);
     }
 }
-
-
-
-// let dogs = 500
-// let letsRock() = setInterval(() => sequence(), 500)
-// console.log(letsRock)
-
-
+//load page
 const initialize = event => {
     addClickToGrid(gridBox, currentGrid)
 }
-
-submitButton.addEventListener('click', () => {
-    checkForWin(currentGrid,winGridLOneROne)
-})
-
-playButton.addEventListener('click', () => {
-    console.log('play beat button')
-})
-
-
-
-const addClickToGrid = (box, current) =>{
-for(let i = 0; i<box.length; i++){
-    box[i].addEventListener('click',() => {
-        if (current[i] === 0){
-            addFill(box, i, current)}
-        else{
-            removeFill(box, i, current)}
-        })
-    }
-}
-
+//checks for a win
 const checkForWin = (current, win) => {
     if (current.length !== win.length) return false;
 	for (var i = 0; i < current.length; i++) {
-		if (current[i] !== win[i]){ console.log('lost'); return false;}
+		if (current[i] !== win[i]){ alert('Try Again!'); return false;}
 	}
-	console.log('shit yea')
-};
+        alert(`That is the correct beat!`);console.log('shit yea')
+        level ++
+        // console.log(level)
+        resetBoard()
+        clearLevels()   
+        changeLevelHeader()
 
+};
+//resets board to default
+const resetBoard = () => {
+    for(i=0; i<currentGrid.length;i++){
+        gridBox[i].classList.remove('fill', 'active')
+        currentGrid[i] = 0
+    }
+}
+//adds color fill to clicked box and updates class number in array 
 const addFill = (box, i, current) => {
     box[i].classList.add('fill', 'active')
     current[i] = 1
@@ -126,7 +129,7 @@ const removeFill = (box, i, current) => {
     box[i].classList.remove('fill', 'active')
     current[i] = 0
 }
-
+//when you click on a box it makes the sound of the corresponding instrument 
 addSoundOnClick = (box, i) => { 
     if(box[i].classList.contains('hh')){
         hhSound.currentTime = 0
@@ -145,11 +148,39 @@ addSoundOnClick = (box, i) => {
         rideSound.play();
     }
 }
-
+//on click you can hear what you created back 
 playBackButton.addEventListener('click', function (){ 
     console.log('play back button ')
     rock()
-
 })
+//on click it will play the beat you're supposed to recreate
+playButton.addEventListener('click', () => {
+    console.log('play beat button')
+})
+//checks to see if what you created is correct 
+submitButton.addEventListener('click', () => {
+    levelAnswer=eval(`winLevel${level}`)
+    checkForWin(currentGrid, levelAnswer)
+})
+//lets the grid to become clickable 
+const addClickToGrid = (box, current) =>{
+    for(let i = 0; i<box.length; i++){
+        box[i].addEventListener('click',() => {
+            if (current[i] === 0){
+                addFill(box, i, current)}
+            else{
+                removeFill(box, i, current)}
+            })
+        }
+    }
 
-document.addEventListener('DOMContentLoaded', initialize);
+document.addEventListener('DOMContentLoaded', initialize);  
+
+clearLevels = () => {
+    if (level === 3){
+        levelContainer.classList.add('hidden')
+    }
+}
+changeLevelHeader = () => {
+    levelTitle.innerHTML=`Level ${level}`
+}   
